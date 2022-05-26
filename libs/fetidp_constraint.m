a633848e65f5a073173c0b2327c13d2c3e99da1a
VL <vl1102@web.de>
2022-05-26 16:10:46 +0200

    VALENTIN: Adaptive Nebenbedingung eingebaut. Deflation scheint noch nicht ganz zu funktionieren (für bestimmte n)

--- libs/fetidp_constraint.m
+++ libs/fetidp_constraint.m
@@ -1,7 +1,7 @@
 function [cu,u_FETIDP_glob,lambda,iter,kappa_est] = fetidp_constraint(vert__sd,tri__sd,l2g__sd,f,dirichlet,VK,constraint_type,rhoTri,rhoTriSD,maxRhoVert,vertTris,logicalTri__sd,plot)
 % constraint types (constraint_type): 
 % 'none' (Vanilla FETIDP)
-% 'non_adaptive' (Aufgabe Teil 1), 
+% 'non-adaptive' (Aufgabe Teil 1), 
 % 'adaptive' (Aufgabe Teil 2)
 if ~(strcmp('Deflation',VK) || strcmp('Balancing',VK))
     constraint_type = 'none';
@@ -166,83 +166,213 @@ temp = f_PiTilde - apply_2(cb_B_trans,cK_BB,cK_PiB,1);
 temp = apply_1(cB_B,cK_BB,cK_PiB,S_PiPi \ temp);
 d = d - temp;
 
-%% Erstelle Kantenlisten
-cEdgesSD = cell(1,1);
-for i = 1:length(cLM)
-    cEdgesSD{1} = [cEdgesSD{1};cLM{i}(1,:)];
-end
-edgesSD = unique(cEdgesSD{1},'rows'); % Enthaelt die beiden angrenzenden Teilgebietsnummern pro TG-Kante
-numEdges = size(edgesSD,1);
-
-edgesDualGlobalAll = cell(size(edgesSD));
-edgesDual = cell(numEdges,1);
-edgesDualGlobal = cell(numEdges,1);
-
-edgesGammaGlobalAll = cell(size(edgesSD));
-edgesGammaGlobal = cell(numEdges,1);
-edgesGamma = cell(numEdges,1);
-
-edgesPrimalGlobalAll = cell(size(edgesSD));
-edgesPrimalGlobal = cell(numEdges,1);
-edgesPrimal = cell(numEdges,1);
-
-for i = 1:numEdges % Iteriere ueber TG-Kanten
-    for j = 1:size(edgesSD,2) % Iteriere ueber angrenzende TG
-        SD = edgesSD(i,j);
-        edgesDualGlobalAll{i,j} = l2g__sd{SD}(cDual{SD});   % Enthaelt fuer jedes angrenzende TG die dualen Knoten (GLOBALE Knotennummern)
-        edgesGammaGlobalAll{i,j} = l2g__sd{SD}(cGamma{SD});
-        edgesPrimalGlobalAll{i,j} =  l2g__sd{SD}(cPrimal{SD});
+%% Nebenbedingung Vorarbeit
+if strcmp(constraint_type,'adaptive') || strcmp(constraint_type,'non-adaptive')
+    %% Erstelle Kantenlisten
+    cEdgesSD = cell(1,1);
+    for i = 1:length(cLM)
+        cEdgesSD{1} = [cEdgesSD{1};cLM{i}(1,:)];
     end
-    edgesGammaGlobal{i} = intersect(edgesGammaGlobalAll{i,1},edgesGammaGlobalAll{i,2});
-    edgesGamma{i} = mapGamma(edgesGammaGlobal{i});
-    edgesDualGlobal{i} = intersect(edgesDualGlobalAll{i,1},edgesDualGlobalAll{i,2}); % Enthaelt fuer die TG-Kanten die dualen Knoten (GLOBALE Knotennummern)
-    edgesDual{i} = mapDual(edgesDualGlobal{i}); % Enthaelt fuer die TG-Kanten die dualen Knoten (DUALE Knotennummern)
-    edgesPrimalGlobal{i} = intersect(edgesPrimalGlobalAll{i,1},edgesPrimalGlobalAll{i,2});
-    edgesPrimal{i} = mapPi(edgesPrimalGlobal{i});
-end
-% Umkehrabbildung
-g2l__sd = cell(numSD,1);
-for sd = 1:numSD
-    g2l__sd{sd} = zeros(numVert,1);
-    ind = l2g__sd{sd};
-    g2l__sd{sd}(ind) = 1:length(l2g__sd{sd});
-end
-
-cLocalPrimal = cell(size(edgesSD));
-cLocalDual = cell(size(edgesSD));
-cLocalGamma = cell(size(edgesSD));
-for i = 1:numEdges
-    for j = 1:2
-        sd = edgesSD(i,j);
-        cLocalPrimal{i,j} = g2l__sd{sd}(edgesPrimalGlobal{i});
-        cLocalDual{i,j} = g2l__sd{sd}(edgesDualGlobal{i});
-        cLocalGamma{i,j} = g2l__sd{sd}(edgesGammaGlobal{i});
+    edgesSD = unique(cEdgesSD{1},'rows'); % Enthaelt die beiden angrenzenden Teilgebietsnummern pro TG-Kante
+    numEdges = size(edgesSD,1);
+    
+    edgesDualGlobalAll = cell(size(edgesSD));
+    edgesDual = cell(numEdges,1);
+    edgesDualGlobal = cell(numEdges,1);
+    
+    edgesGammaGlobalAll = cell(size(edgesSD));
+    edgesGammaGlobal = cell(numEdges,1);
+    edgesGamma = cell(numEdges,1);
+    
+    edgesPrimalGlobalAll = cell(size(edgesSD));
+    edgesPrimalGlobal = cell(numEdges,1);
+    edgesPrimal = cell(numEdges,1);
+    
+    for i = 1:numEdges % Iteriere ueber TG-Kanten
+        for j = 1:size(edgesSD,2) % Iteriere ueber angrenzende TG
+            SD = edgesSD(i,j);
+            edgesDualGlobalAll{i,j} = l2g__sd{SD}(cDual{SD});   % Enthaelt fuer jedes angrenzende TG die dualen Knoten (GLOBALE Knotennummern)
+            edgesGammaGlobalAll{i,j} = l2g__sd{SD}(cGamma{SD});
+            edgesPrimalGlobalAll{i,j} =  l2g__sd{SD}(cPrimal{SD});
+        end
+        edgesGammaGlobal{i} = intersect(edgesGammaGlobalAll{i,1},edgesGammaGlobalAll{i,2});
+        edgesGamma{i} = mapGamma(edgesGammaGlobal{i});
+        edgesDualGlobal{i} = intersect(edgesDualGlobalAll{i,1},edgesDualGlobalAll{i,2}); % Enthaelt fuer die TG-Kanten die dualen Knoten (GLOBALE Knotennummern)
+        edgesDual{i} = mapDual(edgesDualGlobal{i}); % Enthaelt fuer die TG-Kanten die dualen Knoten (DUALE Knotennummern)
+        edgesPrimalGlobal{i} = intersect(edgesPrimalGlobalAll{i,1},edgesPrimalGlobalAll{i,2});
+        edgesPrimal{i} = mapPrimal(edgesPrimalGlobal{i});
     end
-end
-
-
-%% Definiere Matrix U
-U = zeros(n_LM,numEdges);
-for i = 1:numEdges % Iteriere ueber Kanten
-    cnt=1;
-    for j = edgesDual{i}'    % Iteriere ueber duale Knoten der Kante (DUALE Knotennummern)
-        U(j,i) = maxRhoVert(edgesDualGlobal{i}(cnt));
-        cnt = cnt+1;
+    % Umkehrabbildung
+    g2l__sd = cell(numSD,1);
+    for sd = 1:numSD
+        g2l__sd{sd} = zeros(numVert,1);
+        ind = l2g__sd{sd};
+        g2l__sd{sd}(ind) = 1:length(l2g__sd{sd});
     end
+    
+    cLocalPrimal = cell(size(edgesSD));
+    cLocalDual = cell(size(edgesSD));
+    cLocalGamma = cell(size(edgesSD));
+    for i = 1:numEdges
+        for j = 1:2
+            sd = edgesSD(i,j);
+            cLocalPrimal{i,j} = g2l__sd{sd}(edgesPrimalGlobal{i});
+            cLocalDual{i,j} = g2l__sd{sd}(edgesDualGlobal{i});
+            cLocalGamma{i,j} = g2l__sd{sd}(edgesGammaGlobal{i});
+        end
+    end
+
+    %% Definiere Matrix U
+    if strcmp(constraint_type,'non-adaptive')
+        U = zeros(n_LM,numEdges);
+        for edgeID = 1:numEdges % Iteriere ueber Kanten
+            cnt=1;
+            for j = edgesDual{edgeID}'    % Iteriere ueber duale Knoten der Kante (DUALE Knotennummern)
+                U(j,edgeID) = maxRhoVert(edgesDualGlobal{edgeID}(cnt));
+                cnt = cnt+1;
+            end
+        end
+    end
+    if strcmp(constraint_type,'adaptive')
+        cU=cell(1,numEdges);
+        for edgeID = 1:numEdges
+            %% R
+            nPrimal = length(edgesPrimalGlobal{edgeID});
+            nGamma = [nnz(cGamma{edgesSD(edgeID,1)}),nnz(cGamma{edgesSD(edgeID,2)})];
+            nGammaUnass = sum(nGamma);
+            nDual = nGamma-nPrimal;
+            P_e = zeros(nGammaUnass,nPrimal);
+            R_1 = zeros(nGammaUnass,nDual(1));
+            R_2 = zeros(nGammaUnass,nDual(2));
+            % Knotensortierung in Spalten ist:
+            % primal, dual(1), dual(2)
+            % Knotensortierung in Zeilen ist:
+            % primal(1), primal(2), dual(1), dual(2)
+            % Alternative Knotensortierung in Zeilen wäre:
+            % gamma(1), gamma(2) (=primal(1),dual(1),primal(2),dual(2))
+            P_e(1:nPrimal,1:nPrimal) = eye(nPrimal);
+            R_1(nPrimal+1 : nGamma(1),:) = eye(nDual(1));
+
+            P_e(nGamma(1) + 1:nGamma(1)+nPrimal,1:nPrimal) = eye(nPrimal);
+            R_2(nGamma(1)+nPrimal+1 : nGammaUnass,:) = eye(nDual(2));
+            R = [P_e, R_1, R_2];
+            pi = R*((R'*R)\R');
+            % pi hat Dimension nGammaUnass x nGammaUnass
+
+            %% P_D und B
+            % P_D wird Dimension dim(B_D_e,2) x dim(B_e,2) haben
+            % Also muss dim(B_D_e,2) = nGammaUnass entsprechen
+            % Also muss dim(B_e,2) = nGammaUnass entsprechen
+            B_D_e = cell(1,2);
+            B_e = cell(1,2);
+
+            for k = 1:2
+                sd = edgesSD(edgeID,k);
+                B_e{k} = zeros(n_LM,nGamma(k));
+                B_D_e{k} = zeros(n_LM,nGamma(k));
+
+                % Der folgende Index listet alle lokalen Knotenindizes vom
+                % aktuellen Teilgebiet, welche zum Interface gehoeren aber kein
+                % primaler Knoten auf der betrachteten Kante ist. Die primalen
+                % Knoten auf der Kante sind lediglich Nullspalten welche vorne
+                % angefügt werden.
+                relevantGamma = setdiff(find(cGamma{sd}),cLocalPrimal{edgeID,k});
+                B_e{k}(:,nPrimal+1:end) = cB{sd}(:,relevantGamma);
+                B_D_e{k}(:,nPrimal+1:end) = cBskal{sd}(:,relevantGamma);
+            end
+            B_e = cell2mat(B_e);
+            B_D_e = cell2mat(B_D_e);
+
+            % Loesche nun alle Zeilen welche nicht zu einem LM auf der
+            % betrachteten Kante gehoeren.
+            subset = (full(sum(abs(B_e),2)) == 2);
+            B_e = B_e(subset,:);
+            B_D_e = B_D_e(subset,:);
+            P_D_e = B_D_e'*B_e;
+
+            %% S
+            % Option 1: erst die Schurkomplemente normal Teilgebietsweise
+            % berechenen (siehe oben) und anschliessend umsortieren
+            %     S_e = cell(2,1);
+            %     for k = 1:2
+            %         sd = edgesSD(edgeID,k);
+            %         S
+            %     end
+            %     S_alternativ = blkdiag(S_e{:})
+
+            % Option 2: die Schurkomplemente von Anfang an umsortiert berechenen
+            S_e = cell(2,1);
+            for k = 1:2
+                sd = edgesSD(edgeID,k);
+                inner_local = cInner{sd};
+                relevantPrimal = cLocalPrimal{edgeID,k};
+                relevantGamma = setdiff(find(cGamma{sd}),relevantPrimal);
+
+
+                S = cell(2,2);
+
+                % Nur in primalen
+                gamma_1 = relevantPrimal;
+                gamma_2 = relevantPrimal;
+
+                S{1,1} = cK{sd}(gamma_1,gamma_2);
+                temp = cK{sd}(inner_local,inner_local) \ cK{sd}(inner_local,gamma_2);
+                S{1,1} = S{1,1} - cK{sd}(gamma_1,inner_local) * temp;
+
+                % In primalen und restlichen
+                gamma_1 = relevantPrimal;
+                gamma_2 = relevantGamma;
+
+                S{1,2} = cK{sd}(gamma_1,gamma_2);
+                temp = cK{sd}(inner_local,inner_local) \ cK{sd}(inner_local,gamma_2);
+                S{1,2} = S{1,2} - cK{sd}(gamma_1,inner_local) * temp;
+                S{2,1} = S{1,2}';
+
+                % Nur in restlichen
+                gamma_1 = relevantGamma;
+                gamma_2 = relevantGamma;
+
+                S{2,2} = cK{sd}(gamma_1,gamma_2);
+                temp = cK{sd}(inner_local,inner_local) \ cK{sd}(inner_local,gamma_2);
+                S{2,2} = S{2,2} - cK{sd}(gamma_1,inner_local) * temp;
+
+                S = cell2mat(S);
+                S_e{k} = S;
+            end
+            S = blkdiag(S_e{:});
+            sigma = max(diag(S));
+
+
+            %% c
+            c = ones(nGammaUnass,1) / norm(ones(nGammaUnass,1));
+
+            %% Verallgmeinertes Eigenwertproblem loesen
+            [eigenvalues, eigenvectors] = adaptiveEigenvalues(c, pi, P_D_e, S,sigma);
+            TOL = 100;
+            eigenvectors = eigenvectors(:,diag(eigenvalues) > TOL);
+
+            % Extrahiere u
+            n_EV = size(eigenvectors,2);
+            U_temp = zeros(n_LM,n_EV);
+            for k = 1:n_EV
+                U_temp(subset,:) = B_D_e * S * P_D_e * eigenvectors;
+            end
+            cU{edgeID} = U_temp;
+        end
+        U = cell2mat(cU);
+    end
+    
+
+    %% Definiere Projektion P
+    UFU = U'*hF(U);
+    invUFU = UFU\eye(size(UFU));
+    P = @(x) U*invUFU*U'*F(cB_B,cK_BB,cK_PiB,S_PiPi,x);
+    P_transpose = @(x) F(cB_B,cK_BB,cK_PiB,S_PiPi,U*invUFU*U'*x);
+    IminusP = @(x) x-P(x);
+    IminusP_transpose = @(x) x-P_transpose(x);
 end
 
-
-%% Definiere Projektion P
-UFU = U'*hF(U);
-invUFU = UFU\eye(size(UFU));
-P = @(x) U*invUFU*U'*F(cB_B,cK_BB,cK_PiB,S_PiPi,x);
-P_transpose = @(x) F(cB_B,cK_BB,cK_PiB,S_PiPi,U*invUFU*U'*x);
-IminusP = @(x) x-P(x);
-IminusP_transpose = @(x) x-P_transpose(x);
-
-
-%% PCG + Vorkonditionierer neu
-
+%% PCG + Vorkonditionierer
 if strcmp('Identitaet',VK)
     % Identitaet VK
     invM  = @(x) x;
@@ -265,12 +395,16 @@ end
 % EW50 = eigenwerte(1:50);
 
 %% PCG
-tol = 10^(-8);
+tol = 10^(-7);
 x0 = zeros(n_LM,1);
 ploth = @(lambda,iter,VK) plotiter(lambda,iter,VK,cB_B,cK_BB,cK_PiB,cb_B,cPrimalMap, ...
     l2g__sd,cPrimal,cIDual,S_PiPi,f_PiTilde,f_B,tri__sd,vert__sd);
 
-[lambda,~,iter,kappa_est] = preCG(hF,invM,d,x0,tol,VK,ploth,U,invUFU,d);
+if strcmp(constraint_type,'adaptive') || strcmp(constraint_type,'non-adaptive')
+    [lambda,~,iter,kappa_est] = preCG(hF,invM,d,x0,tol,VK,ploth,U,invUFU,d);
+else
+    [lambda,~,iter,kappa_est] = preCG(hF,invM,d,x0,tol,VK,ploth);
+end
 
 % Korrektur bei Deflation-VK notwendig
 if strcmp('Deflation',VK)  % Deflation-VK M^-1_PP
@@ -278,62 +412,14 @@ if strcmp('Deflation',VK)  % Deflation-VK M^-1_PP
     lambda = lambdaBar+lambda;
 end
 
-%% Extrahiere Loesung u_i
+%% Extrahiere Loesung u
 [cu,u_FETIDP_glob] = extract_u(lambda,cB_B,cK_BB,cK_PiB,cb_B,cPrimalMap,...
     l2g__sd,cPrimal,cIDual,S_PiPi,f_PiTilde,f_B);
 
-
-%% PCG + Vorkonditionierer alt
-% %% Definiere Vorkonditionierer
-% dirichletVK = @(x) dirVKfunction(numSD,cBskal_Delta,cK_DeltaDelta,cK_II,cK_DeltaI,x);
-% deflationVK = @(x) IminusP(dirichletVK(IminusP_transpose(x)));
-% balancingVK = @(x) deflationVK(x)+U*invUFU*U'*x;
-%
-% %% PCG
-% tol = 10^(-8);
-% x0 = zeros(n_LM,1);
-% % Funktion zum Plotten der Loesungen waehrend der Iteration von PCG
-% ploth = @(lambda,iter,VK) plotiter(lambda,iter,VK,cB_B,cK_BB,cK_PiB,cb_B,cPrimalMap, ...
-%     l2g__sd,cPrimal,cIDual,S_PiPi,f_PiTilde,f_B,tri__sd,vert__sd);
-%
-% lambda = cell(length(VK),1);
-% iter = cell(length(VK),1);
-% kappa_est = cell(length(VK),1);
-% cu = cell(length(VK),1);
-% u_FETIDP_glob = cell(length(VK),1);
-% for i=1:length(VK)
-%     if strcmp('Deflation',VK{i})  % Deflation-VK M^-1_PP
-%         invM  = @(x) deflationVK(x);
-%     elseif strcmp('Balancing',VK{i}) % Balancing-VK M^-1_BP
-%         invM  = @(x) balancingVK(x);
-%     elseif strcmp('Dirichlet',VK{i})  % Dirichlet-VK
-%         invM  = @(x) dirichletVK(x);
-%     elseif strcmp('Identitaet',VK{i})    % Identitaet
-%
-%     end
-%
-%     %% Eigenwerte berechnen (50 groessten)
-%     invMF = invM(hF(eye(size(U,1))));
-%     eigenwerte = eig(invMF);
-%     eigenwerte = sort(eigenwerte,'descend');
-%     EW50 = eigenwerte(1:50);
-%
-%     [lambda{i},~,iter{i},kappa_est{i}] = preCG(hF,invM,d,x0,tol,VK{i},ploth,U,invUFU,d);
-%
-%     % Korrektur bei Deflation-VK notwendig
-%     if strcmp('Deflation',VK{i})  % Deflation-VK M^-1_PP
-%         lambdaBar = U*invUFU*U'*d;
-%         lambda{i} = lambdaBar+lambda{i};
-%     end
-%
-%     %% Extrahiere Loesung u_i
-%     [cu{i},u_FETIDP_glob{i}] = extract_u(lambda{i},cB_B,cK_BB,cK_PiB,cb_B,cPrimalMap,...
-%         l2g__sd,cPrimal,cIDual,S_PiPi,f_PiTilde,f_B);
-%
-% end
-
 end
 
+
+%% Plot und Extraktionsfunktionen
 function [cu,u_glob] = extract_u(lambda,cB_B,cK_BB,cK_PiB,cb_B,cPrimalMap,...
     l2g__sd,cPrimal,cIDual,S_PiPi,f_PiTilde,f_B)
 numSD = length(cB_B);
@@ -419,4 +505,28 @@ for i=1:numSD
     Vec2=S_DeltaDeltaiFct(cK_DeltaDelta{i},cK_II{i},cK_DeltaI{i},Vec1);
     ergebnis=ergebnis+cBskal_Delta{i}*Vec2;
 end
+end
+
+function [eigenvalues, eigenvectors] = adaptiveEigenvalues(c, pi, P_D, S,sigma)
+% A = Pi*P_D^T*S*P_D*Pi (Korrektur notwendig?)
+% Fall 1: c ist NICHT im Kern von S:
+% Setze B = B_tilde = Pi * S * Pi + sigma * (I-Pi)
+% Fall 2: c ist im Kern von S:
+% Setze B = Pibar * B_tilde * Pibar + sigma * (I-Pibar)
+
+% LHS = pi_bar * pi * P_D' * S * P_D * pi * pi_bar;
+A =  pi * P_D' * S * P_D * pi;
+Btilde = pi * S * pi + sigma*(eye(size(pi))-pi);
+if norm(S*c) < 10^(-8)
+    pibar = eye(nGammaUnass) - c*c';
+    B = pibar * Btilde * pibar + sigma*(eye(size(pibar))-pibar); 
+else
+    B = Btilde;
+end
+% Korrigiere um evenuelle Asymmetrien
+A = 1/2*(A + A');
+B = 1/2*(B + B');
+
+% Loese das Eigenwertproblem
+[eigenvectors,eigenvalues] = eig(A,B);
 end
