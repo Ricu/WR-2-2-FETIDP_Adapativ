d43a4b70606f5b4221156d6f819c51edcc4f9ce8
ahaende2 <angelina.haendeler@web.de>
2022-05-27 11:51:43 +0200

    Angelina: Aufruf mit Residuum geändert; maxRho teilgebietsweise geändert

--- libs/preCG.m
+++ libs/preCG.m
@@ -1,4 +1,4 @@
-function [x,resid,iter,kappa_est,alpha,beta] = preCG(A,invM,b,x0,tol,VK,ploth,U,invUFU,d)
+function [x,residFinal,iter,kappa_est,alpha,beta] = preCG(A,invM,b,x0,tol,resid,VK,ploth,U,invUFU,d)
 
 r0 = b - A(x0);
 rk = r0;
@@ -9,10 +9,16 @@ xk = x0;
 iter = 0;
 alpha_vec = zeros(1000,1);
 beta_vec = zeros(1000,1);
-nanInfo = cell(1000,1);
+
+% Definiere Abbruchbedingung mit Residuum
+if strcmp('vorkonditioniert',resid)
+    termCond = norm(zk)/norm(z0);
+else % nicht-vorkonditioniert
+    termCond = norm(rk)/norm(r0);
+end
 
 figure("Name","Loesungen waehrend der Iteration von PCG")
-while norm(zk)/norm(z0) > tol %norm(rk)/norm(r0) > tol %
+while termCond > tol %norm(rk)/norm(r0) > tol %
     if nargin > 5 && iter < 4
         if strcmp('Deflation',VK)
             xBar = U*invUFU*U'*d;   % Korrektur bei Deflation-VK notwendig
@@ -30,6 +36,7 @@ while norm(zk)/norm(z0) > tol %norm(rk)/norm(r0) > tol %
     rk = rkp1;
     zk = zkp1;
     iter = iter+1;
+    
     if iter > size(alpha_vec)
         alpha_vec = [alpha_vec ; zeros(1000,1)];
         beta_vec = [beta_vec ; zeros(1000,1)];
@@ -37,27 +44,33 @@ while norm(zk)/norm(z0) > tol %norm(rk)/norm(r0) > tol %
     alpha_vec(iter) = ak;
     beta_vec(iter) = bk;
     
-    nanInfo{iter} = isnan(xk);
-%     fprintf('Iter %i, Residuum %10f, Anzahl NaN %i\n',iter,norm(rk)/norm(r0),nnz(isnan(xk)))
+    if strcmp('vorkonditioniert',resid)
+        termCond = norm(zk)/norm(z0);
+    else % nicht-vorkonditioniert
+        termCond = norm(rk)/norm(r0);
+    end
 end
 
 x = xk;
-resid = rk;
+if strcmp('vorkonditioniert',resid)
+    residFinal = zk;
+else % nicht-vorkonditioniert
+    residFinal = rk;
+end
 alpha = alpha_vec(1:iter);
 beta = beta_vec(1:iter);
-nanInfo = nanInfo(1:iter);
 
 if nargout > 3
     temp1 = [sqrt(beta(1:end-1))./alpha(1:end-1); 0];
     temp2 = (1./alpha) + [0;beta(1:iter-1)]./[1;alpha(1:iter-1)];
     temp3 = [0; sqrt(beta(1:end-1))./alpha(1:end-1)];
     Tk = spdiags([temp1 temp2 temp3],-1:1,iter,iter);
-    if ~strcmp('Deflation',VK)
-        kappa_est = cond(full(Tk));
-    else
+    if nnz(isnan(x)) > 0
         kappa_est = 0;
+        fprintf('Die Konditionszahl beim %s-Vorkonditionierer konnte nicht berechnet werden, da die Loesung NaN enthaelt.\n',VK)
+    else
+        kappa_est = cond(full(Tk));
     end
-    kappa_est = cond(full(Tk));
 end
 end
 
--- main.m
+++ main.m
@@ -7,6 +7,13 @@ VK_vec = {'Identitaet',...
           'Deflation',...
           'Balancing'...
           };
+      
+ %% Parameter fuer PCG
+x0 = @(dim) zeros(dim,1); % Startwert
+tol = 10^(-8); % Toleranz
+% Residuum fuer die Abbruchbedingung bei PCG
+%resid = {'vorkonditioniert'}; 
+resid = {'nicht-vorkonditioniert'};
 
 %% Erstelle das Gitter
 n = 10; % 2*n^2 Elemente pro Teilgebiet
@@ -26,14 +33,14 @@ dirichlet = or(ismember(vert(:,1),xyLim), ismember(vert(:,2),xyLim));
 %% PDE
 f = @(vert,y) ones(size(vert));   % Rechte Seite der DGL
 
-%% Definiere Koeffizientenfunktion (pro Element)
-rhoTri = ones(numTri,1);
-% Definiere Kanal
+%% Definiere Koeffizientenfunktion pro Element
+% Definiere Knotennummern des Kanals
 xMin=14/30; xMax=16/30;
 yMin=3/30;  yMax=27/30;
 indVertCanal = (xMin <= vert(:,1) & vert(:,1) <= xMax & yMin <= vert(:,2) & vert(:,2) <= yMax);  % Logischer Vektor, welche Knoten im Kanal liegen
-numVertCanal = 1:numVert;
-numVertCanal = numVertCanal(indVertCanal); % Knotennummern der Knoten, die im Kanal liegen
+numVertCanal = find(indVertCanal); % Knotennummern der Knoten, die im Kanal liegen
+
+rhoTri = ones(numTri,1); % Außerhalb des Kanals entspricht die Koeffizientenfunktion 1
 for i=1:numTri % Iteriere ueber die Elemente
     if ismember(tri(i,:),numVertCanal) % Alle Knoten des Elements liegen im Kanal
         rhoTri(i)=10^6;    % Im Kanal entspricht die Koeffizientenfunktion 10^6
@@ -41,17 +48,16 @@ for i=1:numTri % Iteriere ueber die Elemente
 end
 indElementsCanal = rhoTri > 1; % Logischer Vektor, welche Elemente im Kanal liegen
 
-%% Definiere maximalen Koeffizienten pro TG 
+% Definiere Koeffizientenfunktion fuer Elemente eines TG
 rhoTriSD = cell(numSD,1);
-maxRhoSD = zeros(numSD,1);
 for i = 1:numSD
     rhoTriSD{i} = rhoTri(logicalTri__sd{i});
-    maxRhoSD(i) = max(rhoTriSD{i});
 end
 
-%% Definiere maximalen Koeffizienten pro Knoten
+%% Definiere maximalen Koeffizienten pro Knoten (und teilgebietsweise)
 maxRhoVert = zeros(numVert,1);
 vertTris = cell(numVert,1); 
+maxRhoVertSD = cell(numVert,1);
 for i = 1:numVert % Iteriere ueber Knoten
     iVec = i*ones(1,size(tri,2));
     cnt = 1;
@@ -62,7 +68,13 @@ for i = 1:numVert % Iteriere ueber Knoten
             cnt = cnt+1;
         end
     end
-    maxRhoVert(i) = max(rhoTri(vertTris{i}));
+    maxRhoVert(i) = max(rhoTri(vertTris{i})); % maximaler Koeffizient pro Knoten
+    
+    % Definiere maximalen Koeffizienten pro Knoten teilgebietsweise
+    for k = 1:numSD
+        vertTrisSD = logicalTri__sd{k}(vertTris{i}); % logischer Vektor welche Dreiecke des Knotens im TG liegen
+        maxRhoVertSD{i} = [maxRhoVertSD{i},max(rhoTri(vertTris{i}(vertTrisSD)))]; % maximalen Koeffizienten pro Knoten teilgebietsweise
+    end
 end
 
 %% Plotten des Gitters mit Kanal
@@ -95,10 +107,9 @@ fig_VK_comp = figure("Name","Loesungen fuer verschiedene Vorkonditionierer");
 tiledlayout('flow')
 for vk_ind = 1:length(VK_vec)
     VK = VK_vec{vk_ind};
-    [cu,u_FETIDP_glob,~,iters{vk_ind},kappa_ests{vk_ind}] = fetidp(vert__sd,tri__sd,l2g__sd,f,...
-                                                 dirichlet,VK,rhoTri,rhoTriSD,...
-                                                 maxRhoVert,vertTris,...
-                                                 logicalTri__sd,true);
+    [cu,u_FETIDP_glob,~,iters{vk_ind},kappa_ests{vk_ind}] = fetidp(vert__sd,tri__sd,l2g__sd,...
+                                    f,dirichlet,VK,rhoTriSD,maxRhoVert,maxRhoVertSD,tol,x0,resid);
+                                                 
     diffs{vk_ind} = norm(u_FETIDP_glob-u_ref);
 
     figure(fig_VK_comp)
