f448062982f1f64a349ee7f38e37471a149b43df
VL <vl1102@web.de>
2022-06-05 12:49:48 +0200

    VALENTIN: Kommentare hinzugefügt

--- libs/fetidp.m
+++ libs/fetidp.m
@@ -251,36 +251,36 @@ if strcmp(constraint_type,'adaptive') || strcmp(constraint_type,'non-adaptive')
             nPrimal = length(edgesPrimalGlobal{edgeID}); % Anzahl primaler Knoten auf der Kante
             nGamma = [nnz(cGamma{edgesSD(edgeID,1)}),nnz(cGamma{edgesSD(edgeID,2)})]; % Anzahl Interfaceknoten pro TG
             nGammaUnass = sum(nGamma); % Anzahl unassemblierte Interface Knoten beider TG
-            nDual = nGamma-nPrimal; % Anzahl unassemblierter dualer Knoten beider TG
-            P_e = zeros(nGammaUnass,nPrimal); % Teilassemblierungsmatrix
-            R_1 = zeros(nGammaUnass,nDual(1));
-            R_2 = zeros(nGammaUnass,nDual(2));
+            nRest = nGamma-nPrimal; % Anzahl unassemblierter dualer Knoten beider TG
+            % Teile die Matrix R in drei Teile auf:
             % Knotensortierung in Spalten ist:
             % primal, rest(1), rest(2)
             % Knotensortierung in Zeilen ist:
             % gamma(1), gamma(2) (=primal(1),rest(1),primal(2),rest(2))
-            % Alternative Knotensortierung in Zeilen wäre:
-            % primal(1), primal(2), rest(1), rest(2)
-            P_e(1:nPrimal,1:nPrimal) = eye(nPrimal);
-            R_1(nPrimal+1 : nGamma(1),:) = eye(nDual(1));
+            P_e = zeros(nGammaUnass,nPrimal); % primal
+            R_1 = zeros(nGammaUnass,nRest(1)); % rest(1)
+            R_2 = zeros(nGammaUnass,nRest(2)); % rest(2)
 
-            P_e(nGamma(1) + 1:nGamma(1)+nPrimal,1:nPrimal) = eye(nPrimal);
-            R_2(nGamma(1)+nPrimal+1 : nGammaUnass,:) = eye(nDual(2));
-            R = [P_e, R_1, R_2];
-            pi = R*((R'*R)\R');
+            P_e(1:nPrimal,1:nPrimal) = eye(nPrimal); %primal(1),primal
+            R_1(nPrimal+1 : nGamma(1),:) = eye(nRest(1)); %rest(1),rest(1)
+
+            P_e(nGamma(1) + 1:nGamma(1)+nPrimal,1:nPrimal) = eye(nPrimal); %primal(2),primal
+            R_2(nGamma(1)+nPrimal+1 : nGammaUnass,:) = eye(nRest(2)); % rest(2),rest(2)
+            R = [P_e, R_1, R_2]; % Zusammensetzen
+            pi = R*((R'*R)\R'); 
             % pi hat Dimension nGammaUnass x nGammaUnass
 
-            %% P_D und B
+            %% Sprungoperator und Projektion B und P_D
             % P_D wird Dimension dim(B_D_e,2) x dim(B_e,2) haben
             % Also muss dim(B_D_e,2) = nGammaUnass entsprechen
             % Also muss dim(B_e,2) = nGammaUnass entsprechen
-            B_D_e = cell(1,2);
-            B_e = cell(1,2);
+            B_D = cell(1,2);
+            B = cell(1,2);
 
             for k = 1:2
                 sd = edgesSD(edgeID,k);
-                B_e{k} = zeros(n_LM,nGamma(k));
-                B_D_e{k} = zeros(n_LM,nGamma(k));
+                B{k} = zeros(n_LM,nGamma(k));
+                B_D{k} = zeros(n_LM,nGamma(k));
 
                 % Der folgende Index listet alle lokalen Knotenindizes vom
                 % aktuellen Teilgebiet, welche zum Interface gehoeren aber kein
@@ -288,77 +288,84 @@ if strcmp(constraint_type,'adaptive') || strcmp(constraint_type,'non-adaptive')
                 % Knoten auf der Kante sind lediglich Nullspalten welche vorne
                 % angefuegt werden.
                 relevantGamma = setdiff(find(cGamma{sd}),cLocalPrimal{edgeID,k});
-                B_e{k}(:,nPrimal+1:end) = cB{sd}(:,relevantGamma);
-                B_D_e{k}(:,nPrimal+1:end) = cBskal{sd}(:,relevantGamma);
+                B{k}(:,nPrimal+1:end) = cB{sd}(:,relevantGamma);
+                B_D{k}(:,nPrimal+1:end) = cBskal{sd}(:,relevantGamma);
             end
-            B_e = cell2mat(B_e);
-            B_D_e = cell2mat(B_D_e);
+            B = cell2mat(B);
+            B_D = cell2mat(B_D);
 
             % Loesche nun alle Zeilen welche nicht zu einem LM auf der
             % betrachteten Kante gehoeren.
-            subset = (full(sum(abs(B_e),2)) == 2);
-            B_e = B_e(subset,:);
-            B_D_e = B_D_e(subset,:);
-            P_D_e = B_D_e'*B_e;
+            subset = (full(sum(abs(B),2)) == 2);
+            B = B(subset,:);
+            B_D = B_D(subset,:);
+            P_D = B_D'*B;
 
-            %% S
-            S_e = cell(2,1);
+            %% Schurkomplement S
+            S_temp = cell(2,1);
+            % Erstelle nacheinander die lokalen Schurkomplemente
             for k = 1:2
                 sd = edgesSD(edgeID,k);
                 inner_local = cInner{sd};
+                % Finde die lokalen Indizes die zu primal und rest gehoeren
                 relevantPrimal = cLocalPrimal{edgeID,k};
                 relevantGamma = setdiff(find(cGamma{sd}),relevantPrimal);
-
+                
+                % Schreibe die Kombination aus primal und rest in ein 2x2
+                % Cell
                 S = cell(2,2);
 
-                % Nur in primalen
-                gamma_1 = relevantPrimal;
-                gamma_2 = relevantPrimal;
+                % Nur in primalen (primal,primal)
+                index_1 = relevantPrimal;
+                index_2 = relevantPrimal;
 
-                S{1,1} = cK{sd}(gamma_1,gamma_2);
-                temp = cK{sd}(inner_local,inner_local) \ cK{sd}(inner_local,gamma_2);
-                S{1,1} = S{1,1} - cK{sd}(gamma_1,inner_local) * temp;
+                S{1,1} = cK{sd}(index_1,index_2);
+                temp = cK{sd}(inner_local,inner_local) \ cK{sd}(inner_local,index_2);
+                S{1,1} = S{1,1} - cK{sd}(index_1,inner_local) * temp;
 
-                % In primalen und restlichen
-                gamma_1 = relevantPrimal;
-                gamma_2 = relevantGamma;
+                % In primalen und restlichen (primal,rest)
+                index_1 = relevantPrimal;
+                index_2 = relevantGamma;
 
-                S{1,2} = cK{sd}(gamma_1,gamma_2);
-                temp = cK{sd}(inner_local,inner_local) \ cK{sd}(inner_local,gamma_2);
-                S{1,2} = S{1,2} - cK{sd}(gamma_1,inner_local) * temp;
+                S{1,2} = cK{sd}(index_1,index_2);
+                temp = cK{sd}(inner_local,inner_local) \ cK{sd}(inner_local,index_2);
+                S{1,2} = S{1,2} - cK{sd}(index_1,inner_local) * temp;
                 S{2,1} = S{1,2}';
 
-                % Nur in restlichen
-                gamma_1 = relevantGamma;
-                gamma_2 = relevantGamma;
+                % Nur in restlichen (rest,rest)
+                index_1 = relevantGamma;
+                index_2 = relevantGamma;
 
-                S{2,2} = cK{sd}(gamma_1,gamma_2);
-                temp = cK{sd}(inner_local,inner_local) \ cK{sd}(inner_local,gamma_2);
-                S{2,2} = S{2,2} - cK{sd}(gamma_1,inner_local) * temp;
+                S{2,2} = cK{sd}(index_1,index_2);
+                temp = cK{sd}(inner_local,inner_local) \ cK{sd}(inner_local,index_2);
+                S{2,2} = S{2,2} - cK{sd}(index_1,inner_local) * temp;
 
                 S = cell2mat(S);
-                S_e{k} = S;
+                S_temp{k} = S;
             end
-            S = blkdiag(S_e{:});
+            S = blkdiag(S_temp{:});
+            % Berechne sigma
             sigma = max(diag(S));
 
 
-            %% c
+            %% Stelle den Vector c auf
             c = ones(nGammaUnass,1) / norm(ones(nGammaUnass,1));
 
             %% Verallgmeinertes Eigenwertproblem loesen
-            [eigenvalues, eigenvectors] = adaptiveEigenvalues(c, pi, P_D_e, S,sigma);
+            [eigenvalues, eigenvectors] = adaptiveEigenvalues(c, pi, P_D, S,sigma);
 
+            % Eigenwerte entsprechend Toleranz auswählen
             eigenvectors = eigenvectors(:,diag(eigenvalues) > adaptiveTOL);
 
-            % Extrahiere u
-            n_EV = size(eigenvectors,2);
-            U_temp = zeros(n_LM,n_EV);
-            for k = 1:n_EV
-                U_temp(subset,:) = B_D_e * S * P_D_e * eigenvectors;
-            end
+            % Extrahiere U
+            % Anzahl an neuen Nebenbedingungen entspricht Anzahl
+            % verbliebener Eigenvektoren/-werte
+            U_temp = zeros(n_LM,size(eigenvectors,2));
+            U_temp(subset,:) = B_D * S * P_D * eigenvectors;
             cU{edgeID} = U_temp;
         end
+        % Stelle U auf indem die lokalen U's entlang der 2. Dimension
+        % verkettet werden
         U = cell2mat(cU);
     end
     fprintf('Anzahl Nebenbedingungen = %i, Spaltenrang = %i\n', size(U,2),rank(U,1e-16));
@@ -390,6 +397,8 @@ else
     end
 end
 
+% Stelle das vorkonditionierte System explizit auf. Anhand dessen EW laesst
+% sich die Kkonditionszahl abschaetzen
 preconditioned_system = invM(hF(eye(n_LM)));
 
 %% PCG
